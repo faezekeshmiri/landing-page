3e9528491f49fb9813199babc02465df5f9e2f34
Your Name <you@example.com>
2023-05-31 19:56:15 +0330

    Add hero description.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="home">
-    
+  <div class="hero d-flex justify-center py-5">
+    <div class="hero-des py-15 text-center">
+      <div class="text-white py-5 text-h2 hero-title">Switzerland Has Never Been So Affordable</div>
+      <div class="text-white py-5 px-5 font-weight-thin">Discover Switzerland. Choose the best program, guides, hotels and restaurants.</div>
+      <div class="py-5">
+        <v-btn class="bg-red-darken-1 px-5" height="50px">Start to explore</v-btn>
+      </div>
+    </div>
+    <div>
+      
+    </div>
   </div>
 </template>
 
@@ -12,3 +21,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.hero-des{
+  width: 40%;
+}
+</style>
