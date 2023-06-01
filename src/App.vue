9d835aa4af6df9115046b8d14b3d9f0ffed58b3a
Your Name <you@example.com>
2023-06-02 00:56:52 +0330

    Add side menu and bg image.

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,65 @@
       <router-link class="mx-5" to="/">Attractions</router-link>
       <router-link class="mx-5" to="/">Themes</router-link>
       <span class="bg-red-darken-1 py-5 px-2">
-        <v-btn variant="text" icon="mdi-menu" class="text-white"></v-btn>
+        <v-btn variant="text" icon="mdi-menu" class="text-white" @click.stop="drawer = !drawer"></v-btn>
       </span>
     </div>
   </nav>
+  <v-layout>
+    <v-navigation-drawer
+        v-model="drawer"
+        location="right"
+        temporary
+        class="bg-purple-darken-4"
+      >
+      <v-list>
+        <v-list-group v-for="(item, index) in items" :key="index"  :value="item.title">
+          <template v-slot:activator="{ props }">
+            <v-list-item
+              dir="rtl"
+              v-bind="props"
+              :prepend-icon="item.icon"
+              :title="item.title"
+            ></v-list-item>
+          </template>
+          <v-list-item
+              v-for="(subitem, i) in item.subItems"
+              :key="i"
+              :title="subitem.title"
+              :prepend-icon="subitem.icon"
+              :value="title"
+              dir="rtl"
+            ></v-list-item>
+        </v-list-group>
+      </v-list>
+      </v-navigation-drawer>
+  </v-layout>
   <div>
     <router-view/>
   </div>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+      items: [
+        {title: 'projects', icon: 'mdi-folder' , subItems: [
+          {title: 'project1', icon: 'mdi-file'},
+          {title: 'project2', icon: 'mdi-file'},
+          {title: 'project3', icon: 'mdi-file'}
+        ]}
+      ]
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
 
 <style>
 #app {
@@ -22,9 +73,11 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: #2c3e50;
+  background-color: #080e21;
   padding: 0px;
   margin: 0px;
+  background-size: 100% 50%;
+  background-image: url(./assets/pexels-cliford-mervil-2469122.jpg);
 }
 
 nav {
