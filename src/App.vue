fc69f1ab608ed1ff936216e1d1b3a37be6944c2e
Your Name <you@example.com>
2023-06-01 22:17:55 +0330

    Add menu btn.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <nav class="d-flex justify-space-between py-5">
-    <div class="logo bg-red mx-5 px-1 py-1">logo</div>
+  <nav class="d-flex justify-space-between py-1">
+    <div class="logo bg-red-darken-1 mx-5 px-1 py-1">logo</div>
     <div>
       <router-link class="mx-5" to="/">Day tours</router-link>
       <router-link class="mx-5" to="/">Multi-day tours</router-link>
       <router-link class="mx-5" to="/">Attractions</router-link>
       <router-link class="mx-5" to="/">Themes</router-link>
+      <span class="bg-red-darken-1 py-5 px-2">
+        <v-btn variant="text" icon="mdi-menu" class="text-white"></v-btn>
+      </span>
     </div>
   </nav>
   <div>
@@ -20,6 +23,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   background-color: #2c3e50;
+  padding: 0px;
+  margin: 0px;
 }
 
 nav {
