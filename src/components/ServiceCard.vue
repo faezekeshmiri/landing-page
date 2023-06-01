8c8bc15c05788aebadf0922557be236261be6fd0
Your Name <you@example.com>
2023-06-01 12:37:19 +0330

    Add card title and icon.

---
+++ src/components/ServiceCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card-container rounded py-2 bg-indigo-lighten-4">
+        <div class="d-flex justify-space-between">
+            <v-list-item>
+                <span class="font-weight-bold text-white">{{title}}</span>
+            </v-list-item>
+            <v-list-item>
+                <v-icon :icon="icon" color="red-darken-1"></v-icon>
+            </v-list-item>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'ServiceCard',
+    props: {
+        title: String,
+        icon: String
+    }
+}
+</script>
+
+<style>
+.card-container{
+    width: 300px;
+    height: 150px;
+    position: relative;
+    overflow: hidden;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,54 @@
 <template>
-  <div class="hero d-flex justify-center py-5">
-    <div class="hero-des py-15 text-center">
-      <div class="text-white py-5 text-h2 hero-title">Switzerland Has Never Been So Affordable</div>
-      <div class="text-white py-5 px-5 font-weight-thin">Discover Switzerland. Choose the best program, guides, hotels and restaurants.</div>
-      <div class="py-5">
-        <v-btn class="bg-red-darken-1 px-5" height="50px">Start to explore</v-btn>
-      </div>
-    </div>
-    <div>
-      
-    </div>
-  </div>
+  <v-container class="hero py-5">
+    <v-row class="py-10" justify="center">
+      <v-col cols="7">
+        <div class="text-white py-5 text-h2 hero-title">
+          Switzerland Has Never Been So Affordable
+        </div>
+        <div class="text-white py-5 px-5 font-weight-thin">
+          Discover Switzerland. Choose the best program, guides, hotels and
+          restaurants.
+        </div>
+        <div class="py-5">
+          <v-btn class="bg-red-darken-1 px-5" height="50px"
+            >Start to explore</v-btn
+          >
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <service-card
+        class="mx-2"
+        v-for="service in services"
+        :key="service.title"
+        :title="service.title"
+        :icon="service.icon"
+      ></service-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-
+import ServiceCard from "@/components/ServiceCard.vue";
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-  }
-}
+    ServiceCard,
+  },
+  data() {
+    return {
+      services: [
+        { title: "Audio Guid", icon: "mdi-music" },
+        { title: "Programs" },
+        { title: "Hotels & Restaurants", icon: "mdi-home" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-.hero-des{
+.hero-des {
   width: 40%;
 }
 </style>
